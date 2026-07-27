aba55b24e23ac7362971f8a944b2e6ae57cb090a
Skip <skip@usecurling.com>
2026-07-27 00:05:31 -0300

    Remove all references to /vmodebrasil route

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,15 +1,8 @@
-import { useLocation, Link } from 'react-router-dom'
-import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
 import { Home } from 'lucide-react'
 
 const NotFound = () => {
-  const location = useLocation()
-
-  useEffect(() => {
-    console.error('404 Error: User attempted to access non-existent route:', location.pathname)
-  }, [location.pathname])
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="text-center px-6">
